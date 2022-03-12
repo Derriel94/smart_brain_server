c4445834f7e4741edfeac29f069bff8de54d4c08
Derriel Collins <derrielcollins96@gmail.com>
2022-03-12 16:27:49 -0600

    fixed registration bug for backend changed functionality to connect to the correct table

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const knex = require('knex');
 
 const postgrestable = knex({
@@ -13,102 +14,88 @@ const postgrestable = knex({
 	}
 });
 
-postgrestable.select('*').from('users').then(data => {
-	console.log(data);
-})
+
 
 const app = express();
-const saltRounds = 10;
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'John@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Jebadiah',
-			email: 'Jebadiah@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		}				
-	]
-};
+
 
 app.get('/', (req, res)=> {
-	res.send('here we go');
-
-});
+  res.send('success');
+})
 
 app.post('/signin', (req, res) => {
-
-	database.users.map(user => {
-		if (req.body.email === database.users[0].email && 
-			req.body.password === database.users[0].password) {
-			return res.json(database.users[0]);
-		} else {
-			res.status(400).json('error logging in');
-		}
-		
-	})
-
-});
-
+  postgrestable.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return postgrestable.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
 
 app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	knex('users')
-	.returning('*')
-	.insert({
-		email: email,
-		name: name, 
-		joined: new Date()
-	}).then(user => {
-		//grabs the last item in thedatabase
-		res.json(user[0]);
-	})
-	.catch(err=> res.status(400).json('unable to register'))
-				
-});
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password, saltRounds);
+    postgrestable.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
+})
 
 app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-		res.status(404).json('no such id bro -_-');
-	}
+  const { id } = req.params;
+  postgrestable.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
-	const { id } = req.body;
-	console.log(id);
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			entries++
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(404).json('user was found');
-	}
+  const { id } = req.body;
+  postgrestable('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
 //process.env chooses a port that is given, if it is given even.

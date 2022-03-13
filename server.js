f503c3fc1eec26a23c5208e0c4438fd46667aaba
Derriel Collins <derrielcollins96@gmail.com>
2022-03-12 18:01:30 -0600

    completed updates from code review, added controllers folder to help seperate concers and make code more readable and able to debug in the future

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const knex = require('knex');
+const register = require('./controllers/Register.js');
+const signin = require('./controllers/Signin.js');
+const profile = require('./controllers/Profile.js');
+const image = require('./controllers/Image.js');
 
 const postgrestable = knex({
 	client: 'pg',
@@ -27,76 +31,13 @@ app.get('/', (req, res)=> {
   res.send('success');
 })
 
-app.post('/signin', (req, res) => {
-  postgrestable.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return postgrestable.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, postgrestable, bcrypt)})
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-    postgrestable.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, postgrestable, bcrypt, saltRounds)})
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  postgrestable.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, postgrestable)})
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  postgrestable('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, postgrestable)})
 
 //process.env chooses a port that is given, if it is given even.
 app.listen(process.env.PORT || 3001, ()=> {
